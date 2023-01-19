1cf4f97029eadd525d3c4974b4534ace43fad323
Leon simintei <mallfs@icloud.com>
2023-01-19 15:21:18 +0300

    assignment complete

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, deleteQuiz }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,6 +8,9 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+  function handleClick(){
+    deleteQuiz(id)
+  }
 
   return (
     <li>
@@ -17,7 +20,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions,setQuestions]=useState([]);
+
+  const apiUrl = "http://localhost:4000/questions";
+
+
+  useEffect(()=>{
+    fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => setQuestions(data))
+  },[])
+
+  // console.log(questions);
+
+  const deleteQuiz = async(id)=>{
+    const res = await fetch(`http://localhost:4000/questions/${id}`,{method: "DELETE",
+  headers:{'content-type': 'application/json','Accept' : 'application/json'}});
+
+  const jsonFile = await res.json()
+  console.log(jsonFile)
+  setQuestions(prevQuiz => prevQuiz.filter(question => question.id !== id))
+  }
+
+  const quizes = questions.map((quiz)=>{
+    return(<QuestionItem key={quiz.id} question={quiz} deleteQuiz={deleteQuiz}/>)
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{quizes}</ul>
     </section>
   );
 }
